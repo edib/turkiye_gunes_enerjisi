f4cf458b1ce65402db2732547c4a97a61afd81bf
Selcuk Demirci <selcuk.demirci@intra.local>
2018-08-27 00:34:47 +0300

    Saatlik toplam enerji hesaplanabiliyor

--- solar1.py
+++ solar1.py
@@ -1,39 +1,60 @@
-# -*- coding: utf-8 -*-
 from pysolar.solar import *
-from datetime import datetime, timedelta
-from tqdm import tqdm
-
+from datetime import datetime,timedelta
 from geopy.geocoders import Nominatim
 import pandas as pd
 import warnings
-warnings.filterwarnings('ignore')
-class timeResolution():
+
+#Tqdm var ise import et yoksa tqdm fonksiyonunu taklit et
+try:
+        from tqdm import tqdm
+except ModuleNotFoundError as e:
+        def tqdm(iterable, *argv):
+                return iterable
+'''
+Enum class
+Zaman seviyelerini tutar
+'''
+class TimeResolution():
 	SECONDS = 1
 	MINUTES = 60
 	HOURS = 3600
 	DAYS = 86400
 
-def timeGenerator(base,timeResolution):
-	for x in range(0,31536000,timeResolution):
+'''
+Verilen base zaman üzerinden yine verilen TimeResolution kadar ileriye Zaman
+üreten generator.
+'''
+def timeGenerator(base,TimeResolution):
+	for x in range(0,31536000,TimeResolution):
 		yield base + timedelta(seconds = x)
 
-		
-geolocator = Nominatim(user_agent="test")
-
+def timeMod(base,delta):
+    seconds = int((base - datetime.min).total_seconds())
+    return timedelta(seconds=seconds % delta.total_seconds())
+#Pysolar'ın verdiği warning'i suppress etmek için eklenen ignore
+warnings.filterwarnings('ignore')
+geolocator = Nominatim(user_agent="turkiye_gunes_enerjisi")
 cityList = pd.read_csv("tr_iller.csv",usecols=['city'])
 whole_year = 24*365
 my_time = datetime(2018,1,1,0,0)
 print("City", "Sun Altitude","Hour,Watt/m2")
 output = []
-selectedTimeResolution = timeResolution.MINUTES
-with open("/dev/null",mode='w',encoding='utf-8') as f:
-        for city in tqdm(cityList['city'].values,desc="Şehirler",unit="Şehir"): #:
-                location = geolocator.geocode(city)
-                # calculate for every hour for 2018
-                t = timeGenerator(my_time,selectedTimeResolution)
-                for my_time in tqdm(iterable=t,desc=city,total=31536000/selectedTimeResolution,unit="#"):
-                        sun_alt = get_altitude(location.latitude, location.longitude, my_time)
-                        if sun_alt > 0 :
-                                print(("Time: {}\tCity: {}\tWatt/m2: {}".format(my_time, city, radiation.get_radiation_direct(my_time, sun_alt))),file=f)
-                        #else:
-                        #        print(("City: {}\tTime: {}\tWatt/m2: {}".format(city, sun_alt, my_time, 0)),file=f)
+CALCULATIONRESOLUTION   = TimeResolution.MINUTES
+TARGETRESOLUTION        = TimeResolution.HOURS
+with open("solartimes.txt",mode='w',encoding='utf-8') as f:
+    for city in tqdm(cityList['city'].values,desc="Şehirler",unit="Şehir"): #:
+        location = geolocator.geocode(city)
+        # calculate for every hour for 2018
+        stepsGenerator = timeGenerator(my_time,CALCULATIONRESOLUTION)
+        targetTimeDelta = timedelta(seconds = TARGETRESOLUTION)
+        totalWattage = 0
+        prev_time = 0
+        for my_time in tqdm(iterable=stepsGenerator,desc=city,total=31536000/CALCULATIONRESOLUTION,ncols=100,unit="#"):
+            if timeMod(my_time,targetTimeDelta).seconds == 0:
+                if totalWattage != 0:
+                    print(("City: {}\tSun Altitude {}\tTime: {}\tWatt/m2: {}".format(city, sun_alt, prev_time, totalWattage)),file=f)
+                prev_time = my_time
+                totalWattage = 0
+            sun_alt = get_altitude(location.latitude, location.longitude, my_time)
+            if sun_alt > 0 :
+                totalWattage += radiation.get_radiation_direct(my_time, sun_alt)
